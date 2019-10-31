c53856d469cdd8ed1b301449bda6725507ef672c
whoismikem <whoismikem@gmail.com>
2019-10-30 22:00:11 -0400

    Add ability to create db

--- microDB_store/basic_query.py
+++ microDB_store/basic_query.py
@@ -5,6 +5,7 @@ IDB_ADDRESS = '192.168.1.10'
 
 try:
     client = InfluxDBClient(host=IDB_ADDRESS, port=8086)
+#    client.create_database(DB_NAME)
     client.switch_database(DB_NAME)
 except:
     print("! Unable to connet to influxDB")
